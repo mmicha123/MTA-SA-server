0e5f4f75723a274856759a64ab5ce99e41b0219c
mmicha <mischerbina+gh@gmail.com>
2019-03-18 00:42:13 +0100

    tuning

---
+++ vehicle/s_vehicleTuning.lua
@@ -0,0 +1,4 @@
+---getVehicleUpgradeOnSlot to save the upgared in toJSON(dies das)
+---https://wiki.multitheftauto.com/wiki/GetVehicleUpgradeOnSlot
+
+---https://wiki.multitheftauto.com/wiki/Vehicle_Upgrades
