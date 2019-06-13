10209d2288cec31d721227a9099f291e44f63c41
mmicha123 <mischerbina+gh@gmail.com>
2019-06-13 15:23:43 +0200

    added VectorAngle check for save of nodes and timeout when dmg to vehicle

--- trafficAI/c_vehicleAI.lua
+++ trafficAI/c_vehicleAI.lua
@@ -13,14 +13,11 @@
 
 
     !!! i am tracking the way the player has followed in the nodes they are building something
-]]--
-
+]] --
 --[[
     todo with data:
             search simular nodes no 
-]]
-
-local NODES = {}
+]] local NODES = {}
 
 local nodeIDcounter = 1
 local nodesToDelete = 10
@@ -33,15 +30,15 @@ local vehicle = nil
 local updateLast = 0
 
 local debugShowLast = 10
-local gLable = guiCreateLabel(910, 200, 154 , 100, "tracking movement", false)
+local gLable = guiCreateLabel(910, 200, 154, 100, "tracking movement", false)
 guiSetVisible(gLable, false)
 
 local vehicleDamage = false
 
+local lastAngle = -1
+
 function startTracking(v)
-    if(not v) then
-        return
-    end
+    if (not v) then return end
 
     vehicle = v
     NODES.pName = getPlayerName(getLocalPlayer())
@@ -50,15 +47,16 @@ function startTracking(v)
 
     updateLast = getTickCount()
 
-    addEventHandler("onClientVehicleDamage", getRootElement(), vehicleDamageHandler)
+    addEventHandler("onClientVehicleDamage", getRootElement(),
+                    vehicleDamageHandler)
     addEventHandler("onClientRender", getRootElement(), trackVehicleMovement)
 end
 addEvent("trafficAI_startTracking", true)
 addEventHandler("trafficAI_startTracking", root, startTracking)
 
-
 function stopTracking(vehicle)
-    removeEventHandler("onClientVehicleDamage", getRootElement(), vehicleDamageHandler)
+    removeEventHandler("onClientVehicleDamage", getRootElement(),
+                       vehicleDamageHandler)
     removeEventHandler("onClientRender", getRootElement(), trackVehicleMovement)
     guiSetVisible(gLable, false)
     outputChatBox("saveNodesToFile")
@@ -67,40 +65,60 @@ end
 addEvent("trafficAI_stopTracking", true)
 addEventHandler("trafficAI_stopTracking", root, stopTracking)
 
-
 function trackVehicleMovement()
-    if(((getTickCount() - updateLast) >= nodeUnloadInterval)) then
+    if (((getTickCount() - updateLast) >= nodeUnloadInterval)) then
         outputChatBox("saveNodesToFile")
         saveNodesToFile()
     end
 
-    if(((getTickCount() - updateLast) >= nodeSaveInterval)) then
-        --check for crash and same pos and other errors
+    if (((getTickCount() - updateLast) >= nodeSaveInterval)) then
+        -- check for crash and same pos and other errors
         local NODE = {ID = 1, pos = {}, prevN = 0}
         local vP = {}
 
         vP.x, vP.y, vP.z = getElementPosition(vehicle)
 
         vP.z = math.floor(vP.z)
-        
-        
-        if(nodeIDcounter >= 5) then
-            --check if vehicle has moved
+
+        if (nodeIDcounter >= 5) then
+            -- check if vehicle has moved
             local pN = NODES[nodeIDcounter - 1].prevN
-            if(NODES[pN].pos.x == vP.x and NODES[pN].pos.y == vP.y) then
+            if (NODES[pN].pos.x == vP.x and NODES[pN].pos.y == vP.y) then
                 updateLast = getTickCount()
                 return
             end
         end
 
-        --check for damage
-        if(vehicleDamage) then
-            exports.message:createMessage("Please don't crash while Tracking", 1, 4000)
+        -- check for damage
+        if (vehicleDamage) then
+            exports.message:createMessage("Please don't crash while Tracking",
+                                          1, 4000)
             nodeIDcounter = nodeIDcounter - nodesToDelete
-            updateLast = getTickCount()
+            updateLast = getTickCount() - 5 * nodeSaveInterval
             vehicleDamage = false
             return
         end
+
+        if (nodeIDcounter > 1) then
+            local vec1 = Vector2(vP.x - NODES[nodeIDcounter - 1].pos.x,
+                                 vP.y - NODES[nodeIDcounter - 1].pos.y)
+            local vec2 = Vector2(1, 0)
+
+            -- (test) check for dif in vecort angel 
+            local angle = math.deg(math.acos(
+                                       vec1:dot(vec2) /
+                                           (vec1.length * vec2.length)))
+
+            if (math.abs(angle - lastAngle) < 5) then
+                -- outputChatBox("angle less then 5 deg overide prev ")
+                nodeIDcounter = nodeIDcounter - 1
+            else
+                lastAngle = angle
+            end
+        end
+
+        vP.x = round(vP.x)
+        vP.y = round(vP.y)
 
         NODE.ID = nodeIDcounter
         NODE.pos = vP
@@ -112,31 +130,27 @@ function trackVehicleMovement()
         updateLast = getTickCount()
     end
 
-    --darw line DEBUG
-    if(#NODES <= 2) then 
-        return
-    end
+    -- darw line DEBUG
+    if (#NODES <= 2) then return end
 
     local showPoints = 2
 
-    if(#NODES > debugShowLast) then
-        showPoints = #NODES - debugShowLast
-    end
+    if (#NODES > debugShowLast) then showPoints = #NODES - debugShowLast end
 
     for i = #NODES, showPoints, -1 do
 
         local prevI = NODES[i].prevN
 
-        dxDrawLine3D(NODES[i].pos.x, NODES[i].pos.y, NODES[i].pos.z, NODES[prevI].pos.x, NODES[prevI].pos.y, NODES[prevI].pos.z, 0xFFFFFFFF, 2, false)
+        dxDrawLine3D(NODES[i].pos.x, NODES[i].pos.y, NODES[i].pos.z,
+                     NODES[prevI].pos.x, NODES[prevI].pos.y, NODES[prevI].pos.z,
+                     0xFFFFFFFF, 2, false)
     end
 end
 
-function vehicleDamageHandler()
-    vehicleDamage = true
-end
+function vehicleDamageHandler() vehicleDamage = true end
 
 function saveNodesToFile()
-    local file = assert(fileCreate("trackdata_"..getTickCount()..".json"))
+    local file = assert(fileCreate("trackdata_" .. getTickCount() .. ".json"))
     fileWrite(file, toJSON(NODES))
     fileClose(file)
 
@@ -144,37 +158,4 @@ function saveNodesToFile()
     nodeIDcounter = 1
 end
 
-
---[[
-            local prevID = NODES[nodeIDcounter - 1].prevN
-
-            if(NODES[NODES[nodeIDcounter - 1].prevN].pos.x == vP.x and NODES[NODES[nodeIDcounter - 1].prevN].pos.y == vP.y) then
-            outputChatBox("has not moved")
-            updateLast = getTickCount()
-            return
-        end
-
-                    for i = #NODES, #NODES - nodesToDelete, -1 do
-                if(NODES[i] ~= nil) then
-                    table.remove(NODES, i)
-                end
-            end
-
-            setTimer(
-        function()
-            vehicleDamage = false
-        end, nodeSaveInterval*2, 1)
-
-        ------------
-        --interpolation!
-                    --check for simularities in nodes
-            if(NODES[pN].pos.x - vP.x < 8 and NODES[pN].pos.x - vP.x >= 1) then
-                vP.x = (NODES[pN].pos.x + vP.x) / 2
-                outputChatBox("x mod "..vP.x)
-            end
-
-            if(NODES[pN].pos.y - vP.y < 8 and NODES[pN].pos.y - vP.y >= 1) then
-                vP.y = (NODES[pN].pos.y + vP.y) / 2
-                outputChatBox("y mod "..vP.y)
-            end
-]]
+function round(num) return tonumber(string.format("%." .. (3 or 0) .. "f", num)) end
